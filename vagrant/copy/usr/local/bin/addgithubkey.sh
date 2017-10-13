6c05996ef8bbe5d453e583604c730ce50d51eb13
Harald Heigl <hh-developing@heigl-online.at>
2017-10-13 11:00:22 +0000

    Small changes to vagrant

--- vagrant/copy/usr/local/bin/addgithubkey.sh
+++ vagrant/copy/usr/local/bin/addgithubkey.sh
@@ -1,2 +1,2 @@
 eval $(ssh-agent -s)
-ssh-add /vagrant/localfiles/ssh/id_rsa
+ssh-add /home/vagrant/.ssh/id_rsa
--- vagrant/provisioning_special.sh
+++ vagrant/provisioning_special.sh
@@ -3,4 +3,7 @@ echo "Start Special"
 add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 aptitude update 
 aptitude install -y ubuntu-make
+
+cp /vagrant/localfiles/ssh/id_rsa* /home/vagrant/.ssh/
+chmod 700 /home/vagrant/.ssh/id_rsa
 echo "End Special"
